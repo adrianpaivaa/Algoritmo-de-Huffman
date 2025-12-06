d1e5b19bf859d60a05201abd8aaa0095d3a18992
adrianpaivaa <adrianabreupaiva@googlemail.com>
2025-12-06 16:47:12 -0300

    Atualização na impressão da árvore serializada

---
+++ main.py
@@ -0,0 +1,127 @@
+import heapq
+import re
+
+#   Classe Nó da Árvore de Huffman
+class Node:
+    def __init__(self, freq=0, palavra=None, esq=None, dir=None):
+        self.palavra = palavra
+        self.freq = freq
+        self.esq = esq 
+        self.dir = dir
+
+    def __lt__(self, other):
+        return self.freq < other.freq
+
+
+#   Função para contar a frequência das palavras
+def contar_frequencia(texto):
+    palavras = re.findall(r"[a-zA-ZÀ-ÿ]+", texto.lower())
+    freq = {}
+    for p in palavras:
+        freq[p] = freq.get(p, 0) + 1
+    return freq
+
+
+#   Função para criar a àrvore
+def criar_arvore(freq_map):
+    heap = []
+
+    # Cria um nó para cada palavra
+    for palavra, freq in freq_map.items():
+        heapq.heappush(heap, Node(palavra=palavra, freq=freq))
+
+    # Constrói a árvore
+    while len(heap) > 1:
+        primeiro = heapq.heappop(heap)
+        segundo = heapq.heappop(heap)
+
+        novo = Node(
+            freq=primeiro.freq + segundo.freq,
+            esq=primeiro,
+            dir=segundo
+        )
+
+        heapq.heappush(heap, novo)
+
+    return heap[0] if heap else None
+
+
+#   Gera código de Huffman para cada palavra
+def gerar_codigos(node, prefixo="", tabela=None):
+    if tabela is None:
+        tabela = {}
+
+    # Nó folha
+    if node.palavra is not None:
+        # Retorna "0" se for uma única palavra
+        tabela[node.palavra] = prefixo if prefixo != "" else "0"
+        return tabela
+
+    # Percorre a esquerda
+    if node.esq:
+        gerar_codigos(node.esq, prefixo + "0", tabela)
+
+    # Percorre a direita
+    if node.dir:
+        gerar_codigos(node.dir, prefixo + "1", tabela)
+
+    return tabela
+
+#   Comprime o texto 
+def comprimir(texto, codigos):
+    palavras = re.findall(r"[a-zA-ZÀ-ÿ]+", texto.lower())
+    bits = "".join(codigos[p] for p in palavras)
+    return bits
+
+
+#   Imprime árvore em forma de texto
+def serializar_arvore(node, lista=None):
+    if lista is None:
+        lista = []
+
+    if node.palavra is not None:
+        lista.append(f"L({node.palavra})")
+    else:
+        lista.append("N")
+        serializar_arvore(node.esq, lista)
+        serializar_arvore(node.dir, lista)
+
+    return " ".join(lista)
+
+def processar():
+    with open("data/input.dat", "r", encoding="utf-8") as f:
+        conteudo = f.read().strip()
+
+    textos = [t.strip() for t in conteudo.split("\n\n")]
+    saida = []
+
+    for i, texto in enumerate(textos, start=1):
+        freq = contar_frequencia(texto)
+        arvore = criar_arvore(freq)
+        codigos = gerar_codigos(arvore)
+        comprimido = comprimir(texto, codigos)
+        arvore_serializada = serializar_arvore(arvore)
+
+        bloco = []
+        bloco.append(f"=== TEXTO {i} ===")
+        bloco.append("\nFrase inicial: ")
+        bloco.append(texto)
+        bloco.append("\nEstrutura da Árvore: ")
+        bloco.append(arvore_serializada)
+
+        bloco.append("\nCódigos de Huffman:")
+        for palavra, codigo in codigos.items():
+            bloco.append(f"{palavra} : {codigo}")
+
+        bloco.append("\nTexto Comprimido: ")
+        bloco.append(comprimido)
+
+        saida.append("\n".join(bloco))
+
+    with open("output.dat", "w", encoding="utf-8") as f:
+        f.write("\n\n".join(saida))
+
+    print("Arquivo output.dat gerado.")
+
+if __name__ == "__main__":
+    processar()
